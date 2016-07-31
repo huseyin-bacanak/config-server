0f1a0c2b8d84ec231e3de5c258e0e50ad8632064
Huseyin Bacanak <hbdevacc@gmail.com>
2016-07-31 17:17:06 +0300

    ok

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -3,23 +3,11 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableConfigServer // important!!
 public class App {
-
-//  @Value("${name}")
-  String name = "World";
-
-  @RequestMapping("/")
-  public String home() {
-    return "Hello " + name;
-  }
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
-
 }
